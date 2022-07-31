eb31750ba670d1f677da747fa464b4dec902954e
Everton José <everton7ej@gmail.com>
2022-07-31 17:42:48 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProductItemComponent } from './components/book-cart/product-list/produc
 import { HttpClientModule } from '@angular/common/http';
 import {BooksService} from './components/book-cart/product-list/product-list.service';
 
+
 @NgModule({
   declarations: [
     AppComponent,
